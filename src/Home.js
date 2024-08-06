7715707ede6b5c4b2f93b7f086031d02d1a4737e
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-06 18:00:50 +0530

    make conditional loading msg

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import BlogList from "./blogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
@@ -12,11 +12,13 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+    { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs = {blogs} title = "Blog"/>}
     </div>
   );
