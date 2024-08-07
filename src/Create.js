b1791fb431185a3157ea920494134d4a710a9a80
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-08 04:37:32 +0530

    Create forms to add blogs

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Mario');
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const blog = {title, body, author};
+    console.log(blog);
+  }
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value = {title}
+          onChange={(e)=>setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value = {body}
+          onChange={(e)=>setBody(e.target.value)}
+        />
+        <label>Blog Author:</label>
+        <select
+        value={author}
+        onChange={(e)=>setAuthor(e.target.value)}>
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
