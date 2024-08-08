82a4e29767d3905b8b80c890c8135193bd7eb63f
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-08 05:44:25 +0530

    Add Delete Blog function

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Mario");
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +19,7 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setIsPending(false);
+      navigate('/');
     });
   };
 
